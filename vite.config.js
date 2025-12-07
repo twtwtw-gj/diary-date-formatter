d26bd83f4f821079cae591866dd61257dc23af8e
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2025-12-07 03:43:02 +0000

    migrate: Vite -> Vike

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import vike from 'vike/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss()
+    tailwindcss(),
+    vike()
   ],
   test: {
     globals: true,
